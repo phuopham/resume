dff64919a9250581349c42975a9b5ee056916b2e
phuopham <108047552+phuopham@users.noreply.github.com>
2023-05-18 15:13:06 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import reactLogo from '/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
+
+function App() {
+  const [resume, setResume] = useState({})
+  useEffect(() => {
+    fetch('resume.json').then(response => {
+      response.json().then(resume => {
+        setResume(resume)
+        console.log(resume)
+      })
+    })
+  }, [])
+  return (
+    <div className='max-w-[1280px] m-auto'>
+      <Navbar />
+      {resume.profile && <Profile data={resume.profile} />}
+      <div>
+        <div>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          {JSON.stringify(resume)}
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </div >
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className='fixed top-0 left-0 right-0 bg-slate-800 text-slate-50 p-2 my-1 print:hidden
+        mx-3 rounded-xl shadow-sm z-10'>
+                <div className='max-w-[1280px] m-auto flex items-center justify-between'>
+                    <div className='font-bold'><a href="/"><span className='text-green-400 uppercase'>Phuong</span>'s RESUME</a></div>
+                    <button className='text-bold uppercase px-3 rounded-full bg-green-400 font-semibold text-slate-900' onClick={() => window.print()}>Print</button>
+                </div>
+            </div>
+            <div className="m-4 p-4"></div>
+        </>
+    )
+}
+
+export default Navbar
